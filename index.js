2aa5f353b7a2262fca4071b18db3d2cb56506395
Noelle Ferrari <ferrari.noelle@gmail.com>
2022-09-09 23:57:57 -0700

    Initial commit and implement basic GET requests
    Answers exercise 3.1 from Full Stack Open. The phonebook app can fetch both the full list of numbers and individual pages for each entry, as well as a VERY basic homepage.

---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require('express')
+const app = express()
+
+let persons = [
+    {
+        'id': 1,
+        'name': 'Arto Hellas',
+        'number': '040-123456'
+    },
+    {
+        'id': 2,
+        'name': 'Ada Lovelace',
+        'number': '39-44-5323523'
+    },
+    {
+        'id': 3,
+        'name': 'Dan Abramov',
+        'number': '12-43-234345'
+    },
+    {
+        'id': 4,
+        'name': 'Mary Poppendieck',
+        'number': '39-23-6423122'
+    }
+]
+
+app.get('/', (req, res) => {
+    res.send('Hello world!')
+})
+
+app.get('/api/persons', (req, res) => {
+    res.json(persons)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+    const id = Number(request.params.id)
+    const person = persons.find(p => p.id === id)
+
+    if(person) {
+        res.json(person)
+    } else {
+        res.status(404).end()
+    }
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})

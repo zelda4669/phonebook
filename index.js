ab5e62a3ae6911491662fe4d92014f3beb7b1a00
Noelle Ferrari <ferrari.noelle@gmail.com>
2022-09-13 23:34:08 -0700

    Add basic logic for POST requests
    Completes Full Stack Open exercise 3.5

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+const { response } = require('express')
 const express = require('express')
 const app = express()
+
+app.use(express.json())
 
 let persons = [
     {
@@ -40,7 +43,7 @@ app.get('/api/persons', (req, res) => {
 })
 
 app.get('/api/persons/:id', (req, res) => {
-    const id = Number(request.params.id)
+    const id = Number(req.params.id)
     const person = persons.find(p => p.id === id)
 
     if(person) {
@@ -48,6 +51,39 @@ app.get('/api/persons/:id', (req, res) => {
     } else {
         res.status(404).end()
     }
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    persons = persons.filter(p => p.id !== id)
+    res.status(204).end()
+})
+
+function getId() {
+    return Math.floor(Math.random() * 1000)
+}
+
+app.post('/api/persons', (req, res) => {
+    const body = req.body
+
+    if(!body.name) {
+        return res.status(400).json({
+            error: 'content missing'
+        })
+    } else if(!body.number) {
+        return res.status(400).json({
+            error: 'content missing'
+        })
+    }
+
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: getId()
+    }
+
+    persons = persons.concat(person)
+    res.json(person)
 })
 
 const PORT = 3001
